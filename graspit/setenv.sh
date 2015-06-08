8b5a283b9e6e425ac2ff216e9d6d9d0bc70c2cdf
Matei Ciocarlie <matei.ciocarlie@columbia.edu>
2015-06-08 10:54:38 -0400

    Pushed graspit wrapper to its own directory; further cleanup

---
+++ graspit/setenv.sh
@@ -0,0 +1,2 @@
+export GRASPIT=`rospack find graspit`/graspit_source
+alias graspit='rosrun graspit graspit'
--- setenv.sh
+++
@@ -1,4 +0,0 @@
-export GRASPIT=`rospack find graspit`/graspit_source
-export GRASPIT_PLUGIN_DIR=`rospack find graspit`/graspit_source/plugins
-
-alias graspit='`rospack find graspit`/bin/graspit'
